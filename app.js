8dddca8ab757cf057d6ceb238a6cf65398466052
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-06 11:12:23 +0600

    dynamic nav section done

---
+++ app.js
@@ -0,0 +1,68 @@
+
+const categoryContainer = document.getElementById("categoryContainer")
+
+
+const loadCategory = () => {
+    fetch("https://news-api-fs.vercel.app/api/categories")
+    .then((res)=>res.json())
+    .then((data) => {
+        const categories = data.categories
+        showCatrgory(categories);
+       
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    
+}
+
+
+const showCatrgory = (categories) =>{
+    categories.forEach(cat => {
+            categoryContainer.innerHTML +=`
+            <li id="${cat.id}" class="hover:border-b-5 border-red-700 cursor-pointer">${cat.title}</li>
+            
+            `
+        });
+}
+
+
+
+
+
+
+
+loadCategory();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2nd way to fetch api with async await funciton
+
+// const loadCategoryAsync = async () => {
+//     try{
+//         const res = fetch("https://news-api-fs.vercel.app/api/categories");
+//         const data = (await res).json();
+//         console/log(data)
+//     }
+//     catch (error){
+//         console.log(error);
+//     }
+// };
